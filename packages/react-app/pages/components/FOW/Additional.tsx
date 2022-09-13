d4588eb19a254e62ac1eee8aa02b599676028255
Ankush263 <ankushbanik263@gmail.com>
2022-09-13 23:34:53 +0530

    update

---
+++ packages/react-app/pages/components/FOW/Additional.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Link from 'next/link';
+import Button from '@mui/material/Button';
+
+
+
+function Additional() {
+
+  const [walletAddr, setWalletAddr] = useState('')
+
+  const Submit = async () => {
+    try {
+      console.log(walletAddr)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const styles = {
+    page: `w-screen h-screen flex flex-col justify-around items-center`,
+    aro: `w-full flex justify-start max-w-md`,
+    info: `flex flex-col w-11/12 justify-start max-w-md`,
+    bigLetter: `flex flex-col w-11/12 justify-around text-slate-400 h-1/5 max-w-md`,
+    inputBox: `w-full bg-slate-300/[.9] shadow-2xl border-white-900/75 p-2 rounded-md`,
+    input: `w-full w-full bg-slate-300/[.0] border-white-900/75 p-1 rounded-md focus:outline-none`,
+    space: `w-10 h-2 rounded-md bg-blue-300 mt-1.5`,
+    submit: `text-white capitalize w-full p-1 max-w-md`,
+  }
+
+  return (
+    <div className={styles.page}>
+      <Link href="/components/FOW/Signup">  
+        <div className={styles.aro}>
+          <ArrowBackIcon color="primary" fontSize='large' />
+        </div>
+      </Link>
+      <div className={styles.info}>
+        <span className='text-3xl font-semibold'>Additional</span>
+        <span className='text-3xl font-semibold'>Information</span>
+      </div>
+      <div className={styles.bigLetter}>
+        <span className='text-2xl font-semibold'>Crypto Wallet Address</span>
+        <div className={styles.inputBox}>
+          <input type="text" placeholder='Enter your crypto wallet address' className={styles.input} onChange={(e) => setWalletAddr(e.target.value)} value={walletAddr} />
+        </div>
+      </div>
+      <div className={styles.space}>
+        <span> </span>
+      </div>
+      <Link href="/components/FOW/ProducerPage">
+        <div className="w-10/12 max-w-md mb-5">
+          <Button variant="contained" className={styles.submit} onClick={Submit}>Submit</Button>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default Additional
