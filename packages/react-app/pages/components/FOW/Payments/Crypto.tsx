39cbfc434f8756c657ad3adc5b04036e37a580a3
Ankush263 <ankushbanik263@gmail.com>
2022-09-15 13:11:58 +0530

    add useRouter

--- packages/react-app/pages/components/FOW/Payments/Crypto.tsx
+++ packages/react-app/pages/components/FOW/Payments/Crypto.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 import PaymentHead from '../PaymentHead'
 import Button from '@mui/material/Button';
 
+import { useRouter } from 'next/router'
+
+
 
 function Crypto() {
+
+  const router = useRouter()
+
+  const data = router.query
+
+  const celoAmount = Number(data.finalAmount) * 1.25
 
   const styles = {
     page: `w-screen h-screen flex flex-col justify-between items-center`,
@@ -12,8 +21,9 @@ function Crypto() {
     priority: `w-11/12 h-1/5 flex flex-col justify-around items-center bg-slate-300/[.9] shadow-xl border-white-900/75 rounded-md`,
     btn: `w-11/12 h-1/6`,
     addr: `w-10/12 h-10 bg-white rounded-xl p-2 flex justify-between items-center`,
-    smallBox: `bg-white rounded-md w-8/12 h-10`,
+    smallBox: `bg-white rounded-md w-8/12 h-10 ml-1`,
     box: `rounded-xl w-20 h-20 bg-white`,
+    boxHold: `w-8/12 flex justify-between items-center`
   }
 
   return (
@@ -24,15 +34,17 @@ function Crypto() {
           <div className="flex flex-col justify-around items-start w-10/12 h-3/5">
             <div className={styles.addr}>
               <span className='font-semibold text-gray-500 text-xs'>0x9304B2hdfsf787sf</span>
-              <div className="border-4 w-10 h-10"></div>
+              <div className="w-12 h-12">
+                <img src="/images/qr-code.png" className='w-full h-full' />
+              </div>
             </div>
-            <div className="w-5/12 flex justify-between items-center">
+            <div className={styles.boxHold}>
               <img src="/images/celo-logo.png" className='w-10 h-10'/>
-              <div className={styles.smallBox}></div>
+              <div className={styles.smallBox}>{celoAmount} CELO</div>
             </div>
-            <div className="w-5/12 flex justify-between items-center">
+            <div className={styles.boxHold}>
               <span className='font-semibold text-gray-500'>USD</span>
-              <div className={styles.smallBox}></div>
+              <div className={styles.smallBox}>{data.finalAmount} USD</div>
             </div>
           </div>
           <div className="h-2/6 w-10/12">
@@ -55,18 +67,18 @@ function Crypto() {
             </div>
             <div className="flex flex-col w-1/4 h-full items-center justify-center">
               <div className={styles.box}></div>
-              <span className='font-semibold text-sm'>Low</span>
+              <span className='font-semibold text-sm'>Normal</span>
             </div>
             <div className="flex flex-col w-1/4 h-full items-center justify-center ">
               <div className={styles.box}></div>
-              <span className='font-semibold text-sm'>Low</span>
+              <span className='font-semibold text-sm'>High</span>
             </div>
           </div>
         </div>
         <div className={styles.btn}>
-          <span className='font-semibold'>Fees: 0.04$</span>
+          <span className='font-semibold'>Fees: {data.finalAmount}$</span>
           <div className="w-full flex justify-center items-center">
-            <Button variant="contained" className='p-2 w-11/12'>Send</Button>
+            <Button variant="contained" className='p-2 w-11/12' onClick={() => console.log(data)}>Send</Button>
           </div>
         </div>
       </div>
