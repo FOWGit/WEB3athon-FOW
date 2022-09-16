7b1083ed2bc311d396f5a96a777bd55de518e6a7
Ankush263 <ankushbanik263@gmail.com>
2022-09-16 16:45:29 +0530

    update

--- packages/react-app/pages/components/FOW/Farm/[FarmName].tsx
+++ packages/react-app/pages/components/FOW/Farm/[FarmName].tsx
@@ -13,9 +13,12 @@ function FarmName() {
   const styles = {
     page: `w-screen h-screen flex flex-col justify-between items-center`,
     nav: `w-full h-1/6 flex justify-between items-center bg-slate-300/[.9] shadow-xl border-white-900/75 max-w-screen-sm`,
-    left: `h-full flex justify-between items-center w-5/12 ml-3 `,
+    left: `h-full flex justify-between items-center w-72 ml-3`,
     row: `w-11/12 h-1/6 bg-slate-300/[.9] shadow-xl border-white-900/75 flex flex-col justify-between items-center p-1 rounded-md max-w-screen-sm`,
-    inner: `w-11/12 h-4/6 rounded-2xl mb-1 bg-white`
+    inner: `w-11/12 h-4/6 rounded-2xl mb-1 bg-white flex justify-between items-center`,
+    round: `w-24 h-24 rounded-full flex justify-center items-center`,
+    circle: `w-16 h-16 rounded-full flex justify-center items-center`,
+    img: `w-full h-full rounded-full`,
   }
 
   return (
@@ -27,6 +30,9 @@ function FarmName() {
               <ArrowBackIcon color='primary' fontSize='large' />
             </div>
           </Link>
+          <div className={styles.round}>
+            <img src={router.query.img} className='w-full h-full rounded-full' />
+          </div>
           <div className="font-semibold">
             {router.query.FarmName}
           </div>
@@ -38,18 +44,45 @@ function FarmName() {
       <Link href="/components/FOW/order/Meat">
         <div className={styles.row}>
           <span className='mr-auto font-semibold text-sm ml-2'>Meat</span>
-          <div className={styles.inner}></div>
+          <div className={styles.inner}>
+            <div className={styles.circle}>
+              <img src="/images/cow.png" className={styles.img} />
+            </div>
+            <div className={styles.circle}>
+              <img src="/images/chicken.png" className={styles.img} />
+            </div>
+            <div className={styles.circle}>
+              <img src="/images/goatpng.png" className={styles.img} />
+            </div>
+            <div className={styles.circle}>
+              <img src="/images/lamb.png" className={styles.img} />
+            </div>
+          </div>
         </div>
       </Link>
       <Link href="/components/FOW/order/Produce">
         <div className={styles.row}>
           <span className='mr-auto font-semibold text-sm ml-2'>Produce</span>
-          <div className={styles.inner}></div>
+          <div className={styles.inner}>
+            <div className={styles.circle}>
+              <img src="/images/cow.png" className={styles.img} />
+            </div>
+            <div className={styles.circle}>
+              <img src="/images/chicken.png" className={styles.img} />
+            </div>
+            <div className={styles.circle}>
+              <img src="/images/goatpng.png" className={styles.img} />
+            </div>
+            <div className={styles.circle}>
+              <img src="/images/lamb.png" className={styles.img} />
+            </div>
+          </div>
         </div>
       </Link>
       <div className="bg-slate-300/[.9] shadow-xl border-white-900/75 p-2 rounded-2xl mb-3">
         <span className='text-sm'>Minimum Order $$</span>
       </div>
+      {/* <button onClick={() => console.log(router.query.img)}>click Me</button> */}
     </div>
   )
 }
